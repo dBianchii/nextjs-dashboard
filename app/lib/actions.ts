c64ecee1191eb9991f0c9bee3fd1370aa17fcc70
Gabriel Bianchi <gdbianchii@gmail.com>
2023-10-28 23:47:24 -0300

    End chapter 12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,6 +28,36 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
- 
+  revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+const UpdateInvoice = InvoiceSchema.omit({ date: true });
+
+export async function updateInvoice(formData: FormData) {
+  const { id, customerId, amount, status } = UpdateInvoice.parse({
+    id: formData.get('id'),
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+const DeleteInvoice = InvoiceSchema.pick({ id: true });
+
+export async function deleteInvoice(formData: FormData) {
+  const id = DeleteInvoice.parse({ id: formData.get('id') }).id;
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
